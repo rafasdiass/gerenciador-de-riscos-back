1d03fc19c99bd8b70c7bd9968b49663a1b5ff6c7
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael.local>
2024-08-15 13:32:41 -0300

    chore: atualizaco repo

--- src/main/java/rafael/gerenciador_de_riscos_backend/GerenciadorDeRiscosBackendApplication.java
+++ src/main/java/rafael/gerenciador_de_riscos_backend/GerenciadorDeRiscosBackendApplication.java
@@ -3,7 +3,7 @@ package rafael.gerenciador_de_riscos_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "rafael.gerenciador_de_riscos_backend", "com.example.gerenciador" })
+@SpringBootApplication(scanBasePackages = {"com.rafael.gerenciador_de_riscos_backend", "com.example.gerenciador"})
 public class GerenciadorDeRiscosBackendApplication {
 
     public static void main(String[] args) {
